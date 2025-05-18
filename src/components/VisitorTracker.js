1f05623b0e1d0746743b0557e274cf18cd1a7859
kaniecko <anieckok@gmail.com>
2025-05-17 19:25:03 -0500

    d

--- src/components/VisitorTracker.js
+++ src/components/VisitorTracker.js
@@ -1,19 +1,21 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 export default function VisitorTracker() {
-    useEffect(() => {
-        const trackVisit = async () => {
-            try {
-                await fetch('/.netlify/functions/track-visitor', {
-                    method: 'POST',
-                });
-            } catch (error) {
-                console.error('Error tracking visit:', error);
-            }
-        };
+    // useEffect(() => {
+    //     const trackVisit = async () => {
+    //         try {
+    //             await fetch('/.netlify/functions/track-visitor', {
+    //                 method: 'POST',
+    //             });
+    //         } catch (error) {
+    //             console.error('Error tracking visit:', error);
+    //         }
+    //     };
+    //
+    //     trackVisit();
 
-        trackVisit();
-    }, []);
+    // console.log('VisitorTracker component mounted');
+    // }, []);
 
     return null;
 }
